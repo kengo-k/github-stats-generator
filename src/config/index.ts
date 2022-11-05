c1ef7ed630cda55254c7072509687c2aeba980fe
kengo-k <kurobanekengo@gmail.com>
2022-11-05 10:43:27 +0900

    config: add github info

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
 export default {
-    port: process.env.PORT || 3000
-}
+  port: process.env.PORT || 3000,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  OWNER: process.env.OWNER,
+};
