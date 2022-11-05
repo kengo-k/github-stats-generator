4d19cb7c4996674ca5c94aea4fb9deed575c4e6a
kengo-k <kurobanekengo@gmail.com>
2022-11-05 10:51:16 +0900

    未使用コード/importを削除

--- src/routes/apis.ts
+++ src/routes/apis.ts
@@ -1,5 +1,4 @@
 import { Response, Request, Router } from "express";
-import { ActiveProjects, Project } from "../models/task";
 import axios from "axios";
 import config from "../config";
 
@@ -40,37 +39,6 @@ router.get("/active_projects", async (req: Request, res: Response) => {
     GITHUB_HEADER
   );
 
-  // // list all repositories
-  // const repos = await axios.get(
-  //   `https://api.github.com/users/${config.OWNER}/repos`,
-  //   GITHUB_HEADER
-  // );
-
-  // // get commit count per repository in this week
-  // let totalCommits = 0;
-  // const projects: Project[] = [];
-  // for (const repo of repos.data) {
-  //   const commits = await axios.get(
-  //     `https://api.github.com/repos/${config.OWNER}/${repo.name}/commits?since=2022-11-01T15:00:00.00&until=2022-11-02T15:00:00.00`,
-  //     GITHUB_HEADER
-  //   );
-  //   const p: Project = {
-  //     repository_name: repo.name,
-  //     commit_count: commits.data.length,
-  //   };
-  //   totalCommits += commits.data.length;
-  //   projects.push(p);
-  // }
-
-  // const data: ActiveProjects = {
-  //   from_date: "2022-11-04",
-  //   to_date: "2022-11-04",
-  //   projects: projects.map((p) => {
-  //     return Object.assign({}, p, {
-  //       ratio: (p.commit_count / totalCommits) * 100,
-  //     });
-  //   }),
-  // };
   res.send(x.data);
 });
 
