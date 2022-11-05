ec37e1c31f74ada99692c5d2f012726d5bada09e
kengo-k <kurobanekengo@gmail.com>
2022-11-05 11:48:13 +0900

    apis.ts: 冗長なレスポンスを整理

--- src/routes/apis.ts
+++ src/routes/apis.ts
@@ -5,8 +5,14 @@ import { callActiveProjectsQuery } from "../graphql/active_projects";
 const router = Router({ mergeParams: true });
 
 router.get("/active_projects", async (req: Request, res: Response) => {
-  const activeProjects = await callActiveProjectsQuery({ login: config.OWNER });
-  res.send(activeProjects.data);
+  const projects = await callActiveProjectsQuery({ login: config.OWNER });
+  // 指定期間内にPUSHされたリポジトリの一覧のみを抽出
+  const recentProjects = projects.data.data.user.repositories.nodes.filter(
+    (n: any) => {
+      return n;
+    }
+  );
+  res.send(recentProjects);
 });
 
 export default router;
