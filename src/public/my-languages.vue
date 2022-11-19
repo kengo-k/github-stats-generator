ef1634a784a349a01006dcd5d07659b55be0df60
kengo-k <kurobanekengo@gmail.com>
2022-11-19 13:17:15 +0900

    言語別チャートのSFC化

--- src/public/my-languages.vue
+++ src/public/my-languages.vue
@@ -1,12 +1,76 @@
 <template>
-  <div>NEKO</div>
+  <div id="chart"></div>
 </template>
 
 <script>
+  const initChart = (chartData) => {
+    const totalSize = chartData.totalSize;
+    const langMap = chartData.languages;
+    const keys = Object.keys(langMap);
+    const seriesData = keys
+      .map((key) => {
+        return {
+          name: key,
+          y: langMap[key].size,
+          color: langMap[key].color,
+        };
+      })
+      .sort((a, b) => {
+        if (a.y > b.y) {
+          return -1;
+        } else if (a.y === b.y) {
+          return 0;
+        } else {
+          return 1;
+        }
+      });
+    Highcharts.chart("chart", {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: "pie",
+      },
+      title: {
+        text: "Used languages in projects",
+      },
+      tooltip: {
+        pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+      },
+      accessibility: {
+        point: {
+          valueSuffix: "%",
+        },
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: "pointer",
+          dataLabels: {
+            enabled: true,
+            format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+          },
+        },
+      },
+      series: [
+        {
+          name: "Brands",
+          colorByPoint: true,
+          data: seriesData,
+        },
+      ],
+    });
+  };
+
   module.exports = {
     data: function() {
       return {
       }
-    }
+    },
+    mounted() {
+      fetch("/api/langs")
+        .then((response) => response.json())
+        .then((data) => initChart(data));
+    },
   }
 </script>
