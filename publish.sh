b4c605dd6e063cbb4ad73fa0d757653ad03d8356
kengo-k <kurobanekengo@gmail.com>
2022-11-22 21:58:27 +0900

    publish.sh: 最新の構成に合わせて変更

--- publish.sh
+++ publish.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
+
+#
+# build backend
+#
+
+# move backend directory and init
+cd backend
+yarn install
+
+# clean dist directory
 rm -rf dist
+
+# build
 yarn tsc
-cp package.json yarn.lock .mdb .env dist
+cp package.json yarn.lock .env ../.mdb dist
+
+#
+# build frontend
+#
+cd ../frontend
+yarn install
+yarn build
+
+cd ../backend
 cp -r src/public dist
 cd dist
 yarn install
