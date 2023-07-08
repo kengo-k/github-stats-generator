d51538f12589954460d14840d4271006142f5d4f
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-08 21:09:27 +0900

    main.rs: unwrapを除去

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,46 @@ use svg::node::element::{Rectangle, Text};
 use std::fs::File;
 use std::io::prelude::*;
 
-fn create_bar_chart(data: &str, width: i32) -> String {
-    let json: serde_json::Value = serde_json::from_str(data).unwrap();
+#[derive(Debug)]
+enum AppError {
+    JsonCreateFailure,
+    JsonExtractValueFailure
+}
+
+trait JsonValueExtension {
+    fn get_value(&self) -> Result<i64, AppError>;
+}
+
+impl JsonValueExtension for serde_json::Value {
+    fn get_value(&self) -> Result<i64, AppError> {
+        self.as_i64().ok_or(AppError::JsonExtractValueFailure)
+    }
+}
+
+impl From<serde_json::Error> for AppError {
+    fn from(_: serde_json::Error) -> Self {
+        AppError::JsonCreateFailure
+    }
+}
+
+fn to_map(json: &serde_json::Value) -> Result<&serde_json::Map<String, serde_json::Value>, AppError> {
+    match json.as_object() {
+        Some(value) => Ok(value),
+        None => {
+            return Err(AppError::JsonCreateFailure);
+        }
+    }
+}
+
+fn create_bar_chart(data: &str, width: i32) -> Result<String, AppError> {
+
+    let json: serde_json::Value = serde_json::from_str(data)?;
+    let json_map = to_map(&json)?;
 
     // 個々の棒グラフの高さを20に固定する。
     let bar_height = 20;
 
-    let view_height = json.as_object().unwrap().len() as i32 * (bar_height + 10);
-    println!("view_height: {}", view_height);
+    let view_height = json_map.len() as i32 * (bar_height + 10);
 
     // 引数で指定されたwidthを持つSVGを生成する。
     // ただし、高さはデータの数に応じて自動的に決定する。
@@ -24,24 +56,26 @@ fn create_bar_chart(data: &str, width: i32) -> String {
     // jsonの横幅を加工した新しいjsonデータratio_jsonを作成する
     // ratio_jsonの各要素の値はjsonの全要素の値の合計に対する割合となる
     let mut ratio_json = serde_json::Map::new();
+
     let mut sum = 0;
-    for (_, size) in json.as_object().unwrap() {
-        sum += size.as_i64().unwrap();
+    for (_, size) in json_map {
+        sum += size.get_value()?
     }
-    for (language, size) in json.as_object().unwrap() {
-        let ratio = size.as_i64().unwrap() as f64 / sum as f64;
+
+    for (language, size) in json_map {
+        let ratio = size.get_value()? as f64 / sum as f64;
         ratio_json.insert(language.to_string(), serde_json::Value::from(ratio));
     }
     // Mapで作成したratio_jsonをserde_json::Valueに変換する
     let ratio_json = serde_json::Value::from(ratio_json);
-    println!("{:?}", ratio_json);
+    let ration_json_map = to_map(&ratio_json)?;
 
     let mut y = 0;
-    for (language, size) in ratio_json.as_object().unwrap() {
+    for (language, size) in ration_json_map {
         let rect = Rectangle::new()
             .set("x", 100)  // テキストの分だけ棒グラフを右に移動
             .set("y", y)
-            .set("width", size.as_f64().unwrap() * 200.0)
+            .set("width", size.get_value()? as f64 * 200.0)
             .set("height", 20)  // 高さを調整
             .set("fill", "blue");
         document = document.add(rect);
@@ -59,7 +93,7 @@ fn create_bar_chart(data: &str, width: i32) -> String {
     let mut file = File::create("image.svg").unwrap();
     file.write_all(document.to_string().as_bytes()).unwrap();
 
-    document.to_string()
+    Ok(document.to_string())
 }
 
 /// データを取得する関数
@@ -83,7 +117,7 @@ fn get_json() -> String {
 fn index() -> (ContentType, String) {
     let data = &get_json();
     // 例として、SVGデータを動的に生成する関数を呼び出します。
-    let svg_data = create_bar_chart(data, 300);
+    let svg_data = create_bar_chart(data, 300).unwrap();
 
     // 生成されたSVGデータを返す。
     (ContentType::SVG, svg_data)
