ae800a855da40f7f323152e6789b9f3ffab251f2
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-15 23:45:44 +0900

    generate query.rs

--- src/generated/query.rs
+++ src/generated/query.rs
@@ -4,7 +4,7 @@ pub mod github_stats {
     #![allow(dead_code)]
     use std::result::Result;
     pub const OPERATION_NAME: &str = "GithubStats";
-    pub const QUERY : & str = "query GithubStats {\n  viewer {\n    repositories(first: 100) {\n      edges {\n        node {\n          name\n          languages(first: 100) {\n            edges {\n              size\n              node {\n                name\n                color\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}\n" ;
+    pub const QUERY : & str = "query GithubStats {\n  viewer {\n    repositories(isFork: false, first: 100) {\n      edges {\n        node {\n          name\n          languages(first: 100) {\n            edges {\n              size\n              node {\n                name\n                color\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}\n" ;
     use super::*;
     use serde::{Deserialize, Serialize};
     #[allow(dead_code)]
