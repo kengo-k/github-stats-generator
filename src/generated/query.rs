8f7cc42ba4f71c27dc9522e2b436d3af9ed1767e
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-16 14:44:56 +0900

    graphql: スターの数を取得する

--- src/generated/query.rs
+++ src/generated/query.rs
@@ -4,7 +4,7 @@ pub mod github_stats {
     #![allow(dead_code)]
     use std::result::Result;
     pub const OPERATION_NAME: &str = "GithubStats";
-    pub const QUERY : & str = "query GithubStats {\n  viewer {\n    repositories(isFork: false, first: 100) {\n      edges {\n        node {\n          name\n          languages(first: 100) {\n            edges {\n              size\n              node {\n                name\n                color\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}\n" ;
+    pub const QUERY : & str = "query GithubStats {\n  viewer {\n    repositories(isFork: false, first: 100) {\n      edges {\n        node {\n          name\n          stargazerCount\n          languages(first: 100) {\n            edges {\n              size\n              node {\n                name\n                color\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}\n" ;
     use super::*;
     use serde::{Deserialize, Serialize};
     #[allow(dead_code)]
@@ -36,6 +36,8 @@ pub mod github_stats {
     #[derive(Deserialize)]
     pub struct GithubStatsViewerRepositoriesEdgesNode {
         pub name: String,
+        #[serde(rename = "stargazerCount")]
+        pub stargazer_count: Int,
         pub languages: Option<GithubStatsViewerRepositoriesEdgesNodeLanguages>,
     }
     #[derive(Deserialize)]
