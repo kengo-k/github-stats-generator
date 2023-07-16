43956f7609cf8c8379d8626601ea2d92fc64d619
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-16 14:35:58 +0900

    renderer.rs: starアイコンを生成する処理を追加

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,5 +1,5 @@
 use crate::AppError;
-use svg::node::element::{Rectangle, Style, Text};
+use svg::node::element::{Path, Rectangle, Style, Text};
 use svg::Document;
 
 const CSS: &'static str = r#" .top_lang_chart text {
@@ -9,6 +9,21 @@ const CSS: &'static str = r#" .top_lang_chart text {
     font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
     fill: #2f80ed;
 }"#;
+
+const STAR_ICON: &'static str = "M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z";
+
+fn create_star_icon() -> Document {
+    let path = Path::new()
+        .set("d", STAR_ICON)
+        ;
+    let root = Document::new()
+        .set("viewBox", "0 0 16 16")
+        .set("width", 16)
+        .set("height", 16)
+        .add(path)
+        ;
+    root
+}
 
 fn create_bar_chart(lang_name: &str, ratio: f64, color: &str) -> Document {
     const LEFT: i32 = 10;
@@ -86,8 +101,10 @@ fn create_top_lang_chart(data: &Vec<crate::graphql::SvgData>) -> Document {
 
 pub fn write(data: &Vec<crate::graphql::SvgData>) -> Result<String, AppError> {
     let styles = Style::new(CSS);
+    let star = create_star_icon();
     let top_lang_chart = create_top_lang_chart(data);
-    let root = Document::new().add(styles).add(top_lang_chart);
+    let root = Document::new().add(styles).add(top_lang_chart).add(star);
+
 
     Ok(root.to_string())
 }
